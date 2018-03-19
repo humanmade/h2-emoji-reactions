f44ca6ce470cf5c62d38cfe2ce02f2181ee4685f
Ryan McCue <me@ryanmccue.info>
2018-03-19 15:20:18 +1000

    Order comment reactions oldest to newest

--- inc/reactions/inc/class-reaction.php
+++ inc/reactions/inc/class-reaction.php
@@ -241,6 +241,7 @@ class Reaction {
 			'type'    => static::TYPE,
 			'post_id' => $post->ID,
 			'parent'  => $comment->comment_ID,
+			'order'   => 'ASC',
 		];
 		$comments = get_comments( $args );
 		$reactions = static::to_instances( $comments );
